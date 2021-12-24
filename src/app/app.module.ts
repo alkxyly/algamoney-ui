e84f39605f2cd7d84fe0b7bb4d0c6f332af65b89
alkxyly <alkxyly@hotmail.com>
2021-12-24 19:23:08 -0300

    17.2. Criando o serviço de consulta de lançamentos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,34 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FormsModule } from '@angular/forms';
-
-import { ButtonModule } from 'primeng/button';
-import { CalendarModule } from 'primeng/calendar';
-import { InputTextModule } from 'primeng/inputtext';
-import { InputTextareaModule } from 'primeng/inputtextarea';
-import { TableModule } from 'primeng/table';
-import { TabViewModule } from 'primeng/tabview';
-import { TooltipModule } from 'primeng/tooltip';
-import { SelectButtonModule } from 'primeng/selectbutton';
-import { DropdownModule } from 'primeng/dropdown';
-import { InputNumberModule } from 'primeng/inputnumber';
-import { InputMaskModule } from 'primeng/inputmask';
-import { MessageModule } from "primeng/message";
 
 import { AppComponent } from './app.component';
-
-import { NavbarComponent } from './core/navbar/navbar.component';
-
 import { LancamentosModule } from "./lancamentos/lancamentos.module";
 import { PessoasModule } from './pessoas/pessoas.module';
 import { CoreModule } from './core/core.module';
+import { LancamentoService } from './lancamentos/lancamento.service';
 
 
 @NgModule({
   declarations: [
-    AppComponent
-    
+    AppComponent    
   ],
   imports: [
     BrowserModule,
@@ -38,7 +21,7 @@ import { CoreModule } from './core/core.module';
     LancamentosModule,
     PessoasModule
   ],
-  providers: [],
+  providers: [LancamentoService],
   bootstrap: [AppComponent]
 })
 
--- src/app/lancamentos/lancamentos.module.ts
+++ src/app/lancamentos/lancamentos.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 
 import { FormsModule } from '@angular/forms';
 import { ButtonModule } from 'primeng/button';
@@ -27,7 +28,8 @@ import { LancamentosGridComponent } from './lancamentos-grid/lancamentos-grid.co
   imports: [
     CommonModule,
     FormsModule,   
-   
+    HttpClientModule,
+
     SharedModule,
 
     TableModule,
