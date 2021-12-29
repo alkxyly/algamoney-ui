c402abb611b31c4355e4f5097c19222520e56590
alkxyly <alkxyly@hotmail.com>
2021-12-29 13:41:41 -0300

    17.5. Implementando a paginação no serviço de lançamentos

--- src/app/lancamentos/lancamento.service.ts
+++ src/app/lancamentos/lancamento.service.ts
@@ -2,10 +2,12 @@ import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { DatePipe } from '@angular/common';
 
-export interface LancamentoFiltro{
+export class LancamentoFiltro{
   descricao: string;
   dataVencimentoInicio?: Date;
   dataVencimentoFim?: Date;
+  pagina: number = 0;
+  itensPorPagina: number = 5;
 }
 
 @Injectable({
@@ -25,6 +27,9 @@ export class LancamentoService {
       .append('Authorization', 'Basic YWRtaW5AYWxnYW1vbmV5LmNvbTphZG1pbg==');
 
     let params = new HttpParams();
+   
+   params =  params.set('page', filtro.pagina.toString());
+   params = params.set('size', filtro.itensPorPagina.toString());
 
     if (filtro.descricao) {
       params = params.set('descricao', filtro.descricao);
@@ -41,6 +46,14 @@ export class LancamentoService {
   return this.http.get(`${this.lancamentosUrl}?resumo`,
         { headers, params })
       .toPromise()
-      .then((response: any) => response['content']);
+      .then((response: any) => {
+        const lancamentos = response.content;
+        const resultado ={
+          lancamentos,
+          total: response.totalElements
+        };
+
+        return resultado;
+      });
   }
 }
