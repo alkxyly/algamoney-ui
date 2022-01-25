dbcd720276a28c397dc33b6bb47c439b390d4da3
alkxyly <alkxyly@hotmail.com>
2022-01-24 23:56:12 -0300

    18.10. Fazendo redirecionamento

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { PesquisaPessoaComponent } from './pessoas/pesquisa-pessoa/pesquisa-pess
 
 
 const routes: Routes = [
+  { path: '', redirectTo: 'lancamentos', pathMatch: 'full' },
   { path: 'lancamentos', component: LancamentosPesquisaComponent},
   { path: 'lancamentos/novo', component: LancamentoCadastroComponent},
   { path: 'lancamentos/:codigo', component: LancamentoCadastroComponent},
