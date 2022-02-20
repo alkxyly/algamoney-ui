1f219cb1a5cf990a21cca377fac4cb2801dffc82
alkxyly <alkxyly@hotmail.com>
2022-02-20 00:15:18 -0300

    feat: adicionando guard para component de pessoas

--- src/app/pessoas/pessoas-routing.module.ts
+++ src/app/pessoas/pessoas-routing.module.ts
@@ -1,12 +1,25 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
+import { AuthGuard } from "../seguranca/auth.guard";
 import { PesquisaPessoaComponent } from "./pesquisa-pessoa/pesquisa-pessoa.component";
 import { PessoaCadastroComponent } from "./pessoa-cadastro/pessoa-cadastro.component";
 
 const routes: Routes = [
-    { path: 'pessoas', component: PesquisaPessoaComponent},
-    { path: 'pessoas/novo', component: PessoaCadastroComponent},
-    { path: 'pessoas/:codigo', component: PessoaCadastroComponent}
+    { path: 'pessoas', 
+      component: PesquisaPessoaComponent,
+      canActivate: [AuthGuard],
+      data: { roles: ['ROLE_PESQUISAR_PESSOA']}
+    },
+    { path: 'pessoas/novo',
+      component: PessoaCadastroComponent,
+      canActivate: [AuthGuard],
+      data: { roles: ['ROLE_CADASTRAR_PESSOA']}
+    },
+    { path: 'pessoas/:codigo', 
+      component: PessoaCadastroComponent,
+      canActivate: [AuthGuard],
+      data: { roles: ['ROLE_CADASTRAR_PESSOA']}
+    }
 ]
 
 @NgModule({   
